cfb19ccd64ccdf8d8887df552c0554bd66c906f2
Arthur R Castro <arcastroqa@gmail.com>
2026-05-17 22:41:42 -0300

    feat(cypress): index pom

---
+++ frontend/cypress/pages/index.js
@@ -0,0 +1,4 @@
+export { HomePage } from './HomePage'
+export { BuscaPage } from './BuscaPage'
+export { CheckoutPage } from './CheckoutPage'
+export { LpGenericaCursoEscolhido } from './LpGenericaCursoEscolhido'
