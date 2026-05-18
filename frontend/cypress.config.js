218d907c6e5d0ff728c6d53acfe4c96420398ef8
Arthur R Castro <arcastroqa@gmail.com>
2026-05-18 17:53:37 -0300

    chore(cypress): final config

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,17 +1,14 @@
 const { defineConfig } = require("cypress");
-const dataGenerator = require('cypress-test-data-generator');
 
 module.exports = defineConfig({
   allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
-      dataGenerator.registerTasks(on, config);
       return config;
       // implement node event listeners here
     },
     baseUrl: 'https://www.alura.com.br',
-    requestTimeout: 5000,
+    requestTimeout: 10000,
     viewportWidth: 1280,
     viewportHeight: 720,
     video: true,
